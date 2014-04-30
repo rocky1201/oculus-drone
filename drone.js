275a578a1aa2cccfe7ef3fea9d8309dde221c223
Rocky Ongkowidjojo <rocky.ongkowidjojo@sap.com>
2014-04-29 23:48:51 -0700

    added ds4 control

--- drone.js
+++ drone.js
@@ -1,14 +1,28 @@
 'use strict';
 
+var dualShock = require('dualshock-controller');
 var _ = require('lodash');
-var hid = require('node-hid');
-
 
 // Initialize Drone client
 var arDrone  = require('ar-drone'),
-client   = arDrone.createClient(),
-angle    = 128,
-speed    = 0.20;
+    control = arDrone.createUdpControl(),
+    client   = arDrone.createClient(),
+    angle    = 128,
+    speed    = 0.40;
+
+setInterval(function() {
+  // The emergency: true option recovers your drone from emergency mode that can
+  // be caused by flipping it upside down or the drone crashing into something.
+  // In a real program you probably only want to send emergency: true for one
+  // second in the beginning, otherwise your drone may attempt to takeoff again
+  // after a crash.
+  // control.ref({fly: true, emergency: true});
+  // This command makes sure your drone hovers in place and does not drift.
+  control.pcmd();
+  // This causes the actual udp message to be send (multiple commands are
+  // combined into one message)
+  control.flush();
+}, 30);
 
 client.config('control:altitude_max', 500)
 client.config('control:control_vz_max', 1000)
@@ -18,145 +32,230 @@ client.config('control:euler_angle_max', 0.30)
 client.config('control:outdoor', false)
 client.config('control:flight_without_shell', false)
 
-// Init DS4 client
-var rdata = [];
-var devices = hid.devices();
-
-var controller = _(devices)
-.filter(isSonyController)
-.filter(isDS4HID)
-.map(createHID)
-.first();
-
-if (!controller)
-throw new Error('Controller not found');
-
-controller.on('data', function(buf) {
-  rdata = buf;
-});
-
-console.log("Feel free to play with configuration.");
-console.log("Controls:")
-console.log("  Left stick: front/back, left/right");
-console.log("  Right stick: up/down, counterclockwise/clockwise");
-console.log("  O: Takeoff");
-console.log("  X: Land");
-
 var flying   = false,
 hovering = false;
 
-// hid.on('data',function(){
-//     if ((rdata[5] & 32) !== 0){
-//     console.log("x is pressed")
-//     }
+//pass options to init the controller.
+var controller = dualShock(
+    {
+        //you can use a ds4 by uncommenting this line.
+        config: "dualshock4-generic-driver",
+        //if using ds4 comment this line.
+        // config : "dualShock3",
+        //smooths the output from the acelerometers (moving averages) defaults to true
+        accelerometerSmoothing : true,
+        //smooths the output from the analog sticks (moving averages) defaults to false
+        analogStickSmoothing : true
+    });
 
-//     if ((rdata[5] & 64) !== 0){
-//     console.log("o is pressed")
-//     }
 
-//     if ((rdata[5] & 16) !== 0){
-//     console.log("square is pressed")
-//     }
+//make sure you add an error event handler
+controller.on('error', function(data) {
+  //...someStuffDidNotWork();
+});
 
-//     if ((rdata[5] & 128) !== 0){
-//     console.log("triangle is pressed")
-//     }
+//add event handlers:
+controller.on('left:move', function(data) {
+  //...doStuff();
+  moveDrone(data);
+});
+controller.on('right:move', function(data) {
+  //...doStuff();
+  liftDrone(data);
+});
+controller.on('connected', function(data) {
+  //...doStuff();
+  console.log("Connected to Dualshock")
+});
+controller.on('square:press', function (data) {
+  //...doStuff();
+  moveDrone(data);
+});
+controller.on('square:release', function (data) {
+  //...doStuff();
+  moveDrone(data);
+});
 
-// });
+controller.on('triangle:press', function (data) {
+  //...doStuff();
+  moveDrone(data);
+});
+controller.on('triangle:release', function (data) {
+  //...doStuff();
+  moveDrone(data);
+});
 
-setInterval(function() {
-    on("x", ((rdata[5] & 32) !== 0));
-    on("o", ((rdata[5] & 64) !== 0));
-    on("square", ((rdata[5] & 16) !== 0));
-    on("triangle", ((rdata[5] & 128) !== 0));
-    on("left", {x: rdata[1], y: rdata[2]});
-    on("right", {x: rdata[3], y: rdata[4]});
-}, 40);
+controller.on('circle:press', function (data) {
+  //...doStuff();
+  moveDrone(data);
+});
+controller.on('circle:release', function (data) {
+  //...doStuff();
+  moveDrone(data);
+});
 
-function on(type, value) {
-if (type == "x" && value) {
+controller.on('x:press', function (data) {
+  //...doStuff();
+});
+controller.on('x:release', function (data) {
+  //...doStuff();
     if (!flying) {
-        console.log("takeoff");
+        console.log("takeoff, hovering now");
         flying = true;
         client.takeoff(function() {
             hovering = true;
-            console.log("flying");
         });
     }
-}
-
-if (type == "o" && value) {
-    if (flying) {
-        console.log("land");
-        flying   = false;
-        hovering = false;
+    else{
+        console.log("landing now");
         client.land(function() {
-            console.log("landed");
+            flying = false;
+            hovering = false;
             client.stop();
         });
+        
+    }
+});
+
+controller.on('r2:analog', function (data) {
+  //...doStuff();
+    console.log("clockwise:", (data / 255));
+    client.clockwise(data/255);
+});
+controller.on('l2:analog', function (data) {
+  //...doStuff();
+  console.log("cclockwise:", (data / 255));
+    client.counterClockwise(data/255);
+  
+});
+
+
+//connect the controller
+controller.connect();
+
+function moveDrone(data){
+    if(!hovering){
+        console.log("Drone is landed, press X to takeoff")
+        return -1;
+    }
+
+    // _.map(data, function(num){console.log(num);})
+
+    if (data.x<=128){
+        console.log("left:", (128 - data.x)/angle*speed);
+        client.left((128 - data.x) / angle * speed);
+    }
+        
+    else if (data.x>=128){
+        console.log("right:", (data.x - 128) / angle * speed);
+        client.right((data.x - 128) / angle * speed);
+    }
+        
+    
+    if (data.y<=128){
+        console.log("front:", (128 - data.y)/angle*speed);
+        client.front((128 - data.y) / angle * speed);
+    }
+        
+    else if (data.y>=128){
+        console.log("back:", (data.y - 128) / angle * speed);
+        client.back((data.y - 128) / angle * speed);
     }
 }
 
-if (type == "triangle" && value) {
-    if (flying) {
-        console.log("flipAhead");
-        client.animate('flipAhead', 500);
+function liftDrone(data){
+    if(!hovering){
+        console.log("Drone is landed, press X to takeoff")
+        return -1;
+    }
+    if (data.y<=128){
+        console.log("front:", (128 - data.y)/angle*speed);
+        client.up((128 - data.y) / angle * speed);
+    }
+        
+    else if (data.y>=128){
+        console.log("back:", (data.y - 128) / angle * speed);
+        client.down((data.y - 128) / angle * speed);
     }
 }
+// setInterval(function() {
+//     on("x", ((rdata[5] & 32) !== 0));
+//     on("o", ((rdata[5] & 64) !== 0));
+//     on("square", ((rdata[5] & 16) !== 0));
+//     on("triangle", ((rdata[5] & 128) !== 0));
+//     on("left", {x: rdata[1], y: rdata[2]});
+//     on("right", {x: rdata[3], y: rdata[4]});
+// }, 40);
 
-if (type == "square" && value) {
-    if (flying) {
-        console.log("flipBehind");
-        client.animate('flipAhead', 500);
-    }
-}
+// function on(type, value) {
+//     if (type == "x" && value) {
+//         if (!flying) {
+//             console.log("takeoff");
+//             flying = true;
+//             client.takeoff(function() {
+//                 hovering = true;
+//                 console.log("flying");
+//             });
+//         }
+//     }
 
-if (hovering) {
-    if (type == "left" && value.y <= 128) {
-        console.log("front:", (128 - value.y) / angle * speed);
-        client.front((128 - value.y) / angle * speed);
-    } else if (type == "left" && value.y > 128) {
-        console.log("back:", (value.y - 128) / angle * speed);
-        client.back((value.y - 128) / angle * speed);
-    }
+//     if (type == "o" && value) {
+//         if (flying) {
+//             console.log("land");
+//             flying   = false;
+//             hovering = false;
+//             client.land(function() {
+//                 console.log("landed");
+//                 client.stop();
+//             });
+//         }
+//     }
 
-    if (type == "left" && value.x <= 128) {
-        console.log("left:", (128 - value.x) / angle * speed);
-        client.left((128 - value.x) / angle * speed);
-    } else if (type == "left" && value.x > 128) {
-        console.log("right:", (value.x - 128) / angle * speed);
-        client.right((value.x - 128) / angle * speed);
-    }
+//     if (type == "triangle" && value) {
+//         if (flying) {
+//             console.log("flipAhead");
+//             client.animate('flipAhead', 500);
+//         }
+//     }
 
-    if (type == "right" && value.x <= 128) {
-        console.log("counterclockwise:", (128 - value.x) / angle * speed);
-        client.counterClockwise((128 - value.x) / angle * speed);
-    } else if (type == "right" && value.x > 128) {
-        console.log("clockwise:", (value.x - 128) / angle * speed);
-        client.clockwise((value.x - 128) / angle * speed);
-    }
+//     if (type == "square" && value) {
+//         if (flying) {
+//             console.log("flipBehind");
+//             client.animate('flipAhead', 500);
+//         }
+//     }
 
-    if (type == "right" && value.y <= 128) {
-        console.log("up:", (128 - value.y) / angle * speed * 0.2);
-        client.up((128 - value.y) / angle * speed);
-    } else if (type == "right" && value.y > 128) {
-        console.log("down:", (value.y - 128) / angle * speed * 0.2);
-        client.down((value.y - 128) / angle * speed);
-    }
-}
-}
+//     if (hovering) {
+//         if (type == "left" && value.y <= 128) {
+//             console.log("front:", (128 - value.y) / angle * speed);
+//             client.front((128 - value.y) / angle * speed);
+//         } else if (type == "left" && value.y > 128) {
+//             console.log("back:", (value.y - 128) / angle * speed);
+//             client.back((value.y - 128) / angle * speed);
+//         }
 
-// HIDDesciptor -> Boolean
-function isDS4HID(descriptor) {
-  return descriptor.vendorId == 1356 && descriptor.productId == 1476;
-}
+//         if (type == "left" && value.x <= 128) {
+//             console.log("left:", (128 - value.x) / angle * speed);
+//             client.left((128 - value.x) / angle * speed);
+//         } else if (type == "left" && value.x > 128) {
+//             console.log("right:", (value.x - 128) / angle * speed);
+//             client.right((value.x - 128) / angle * speed);
+//         }
 
-// HIDDesciptor -> Boolean
-function isSonyController(descriptor) {
-  return descriptor.manufacturer.match(/^Sony Computer Entertainment/);
-}
+//         if (type == "right" && value.x <= 128) {
+//             console.log("counterclockwise:", (128 - value.x) / angle * speed);
+//             client.counterClockwise((128 - value.x) / angle * speed);
+//         } else if (type == "right" && value.x > 128) {
+//             console.log("clockwise:", (value.x - 128) / angle * speed);
+//             client.clockwise((value.x - 128) / angle * speed);
+//         }
 
-// HIDDescriptor -> HID
-function createHID(descriptor) {
-  return new hid.HID(descriptor.path);
-}
+//         if (type == "right" && value.y <= 128) {
+//             console.log("up:", (128 - value.y) / angle * speed * 0.2);
+//             client.up((128 - value.y) / angle * speed);
+//         } else if (type == "right" && value.y > 128) {
+//             console.log("down:", (value.y - 128) / angle * speed * 0.2);
+//             client.down((value.y - 128) / angle * speed);
+//         }
+//     }
+// }
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var app, client;
 var express = require("express");
-var faye = require("faye");
-var path = require("path");
-var swarm = require("./swarm");
-var _ = require('lodash');
-var hid = require('node-hid');
 
 // Initialize Drone client
 var drone  = require('./drone'),
@@ -16,76 +11,10 @@ app.use('/js', express.static(__dirname+'/public/js'));
 app.use('/css', express.static(__dirname+'/public/css'));
 app.use(express.static(__dirname+ '/public'));
 
-app.get("/drones", function(req, res) {
-  var drones;
-  drones = [];
-  swarm.forEach(function(drone) {
-    return drones.push({
-      id: drone.id,
-      ip: drone.ip,
-      camera: drone.camera,
-      enabled: drone.enabled
-    });
-  });
-  console.log("new client connection (sent %s drones)", drones.length);
-  return res.end(JSON.stringify(drones));
-});
-
-swarm["do"](function(drone) {
-    console.log('config drone:', drone.id);
-    drone.config('general:navdata_demo', 'TRUE');
-    return drone.on('navdata', function(data) {
-      drone.navdata = data;
-      return socket.publish("/drone/navdata/" + drone.id, data);
-    });
-  });
-
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
 
-bayeux = new faye.NodeAdapter({
-    mount: '/faye',
-    timeout: 45
-});
-
-bayeux.attach(server);
-
-bayeux.bind("handshake", function(clientId) {
-  return console.log("socket handshake!", clientId);
-});
-
-bayeux.bind("disconnect", function(clientId) {
-  return console.log("socket disconnect!", clientId);
-});
-
-var socket = new faye.Client("http://localhost:" + server.address().port + "/faye");
-
-socket.subscribe("/drone/enable", function(data) {
-  swarm.drones[data.id].enabled = data.status;
-  return console.log('set drone %s control to %s', data.id, data.status);
-});
-
-socket.subscribe("/drone/camera", function(data) {
-  swarm.drones[data.id].changeCamera(data.camera);
-  return console.log('set drone %s camera to %s', data.id, data.camera);
-});
-
-socket.subscribe("/swarm/move", function(control) {
-  console.log('swarm move', control);
-  return swarm.move(control);
-});
-
-socket.subscribe("/swarm/animate", function(animation) {
-  console.log('swarm animate: ', animation);
-  return swarm.animate(animation);
-});
-
-socket.subscribe("/swarm/action", function(command) {
-  console.log('swarm action: ', command);
-  return swarm.action(command);
-});
-
-require("dronestream").listen(server, {
-   timeout: 500,
-});
+// require("dronestream").listen(server, {
+   // timeout: 500,
+// });
